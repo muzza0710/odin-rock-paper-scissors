885d4a9bd5db69e112dcf55ed8d340361acd12c3
odin <142325575+muzza0710@users.noreply.github.com>
2024-12-03 15:40:34 +0000

    added getHumanChoice function
    prompts user for input rock, paper, scissors and displays if valid

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-console.log('hello world!');
+const choices = ['rock', 'paper', 'scissors'];
 
-function computerChoice(){
-    const choices = ['rock', 'paper', 'scissors'];
+function getComputerChoice(){
     n = Math.floor(Math.random() * 3);
     return choices[n];
 }
 
-console.log(`computer choice: ${computerChoice()}`);
+function getHumanChoice(){
+    let choice = prompt('choose one from: rock, paper, scissors').toLowerCase()
+    if (choices.includes(choice)){
+        return choice;
+    }
+    else {
+        console.warn(choice,` not valid`);
+        return null;
+    }
+
+}
+
+console.log(`Human choice: ${getHumanChoice()}`);
+console.log(`computer choice: ${getComputerChoice()}`);
