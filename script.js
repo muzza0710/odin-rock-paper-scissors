b33b71bebaa7d04f7167661b22550683039f4685
odin <142325575+muzza0710@users.noreply.github.com>
2024-12-03 15:27:53 +0000

    added computerChoice function
    randomly chooses rock, paper or scissors

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('hello world!')
+console.log('hello world!');
+
+function computerChoice(){
+    const choices = ['rock', 'paper', 'scissors'];
+    n = Math.floor(Math.random() * 3);
+    return choices[n];
+}
+
+console.log(`computer choice: ${computerChoice()}`);
